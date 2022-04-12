70a909c2e29c8b86a1ab148ba8c64103b696db82
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-12 15:05:14 -0300

    feat: first example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { appWithTranslation } from 'next-i18next';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
