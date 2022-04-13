25fb59bf35cec0d493476ee2cb6602ea70a99941
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-13 13:07:17 -0300

    perf: add ssr provider for bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@styles/global.css';
 import { appWithTranslation } from 'next-i18next';
-
+import { SSRProvider } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SSRProvider>
+      <Component {...pageProps} />
+    </SSRProvider>
+  );
 }
 
 export default appWithTranslation(MyApp);
