05acb6bc90b8ecc450deb7c69901fe38d89cda68
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-13 16:57:06 -0300

    feat: isolate component

--- src/pages/catalog/create.tsx
+++ src/pages/catalog/create.tsx
@@ -1,13 +1,25 @@
 import { Menu } from '@/modules/Menu/Menu';
+import { FormInputs } from '@/modules/Catalog/FormInputs';
 import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
-const catalog:NextPage = () => {
+const Catalog: NextPage = () => {
   return(
     <>
       <Menu />
-      <main className='container-sm px-5 py-2'>
-        teste
+      <main className='container-sm px-5 py-2 justify-content-stretch'>
+        <Link href="/">
+          <a
+            className="btn btn-primary d-inline-flex gap-2 align-items-center mb-4"
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+            Voltar
+          </a>
+        </Link>
+        <FormInputs />
       </main>
     </>
   );
@@ -21,4 +33,4 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   };
 };
 
-export default catalog;
+export default Catalog;
