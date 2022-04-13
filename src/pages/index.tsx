2bbd042ce9845299a0235edeb8753b475148b5bd
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-13 11:01:00 -0300

    feat: apply menu component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next';
 import { useTranslation }  from 'next-i18next';
 import { serverSideTranslations }  from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
+import { Menu } from '@modules/Menu/Menu';
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Order list</title>
       </Head>
+      <Menu />
       <div className="container-md">
         <h1>{t('MALE')}</h1>
       </div>
