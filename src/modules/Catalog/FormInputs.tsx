6627d9c6e39c506136dd4092360ba77614de3313
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-14 19:08:00 -0300

    refactor: applyind catalog table and add event submit in form

--- src/modules/Catalog/FormInputs.tsx
+++ src/modules/Catalog/FormInputs.tsx
@@ -1,47 +1,46 @@
 import { useCatalog } from '@shared/Catalog/context/catalog';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Container, Form, Col, Row, Tab, Tabs } from 'react-bootstrap';
-import { CatalogTable } from './components/CatalogTable/CatalogTable';
+import { CatalogRef, CatalogTable } from './components/CatalogTable/CatalogTable';
+
+const adultSizes = ['T-PP', 'T-P', 'T-M', 'T-G', 'T-GG', 'T-XG', 'T-2XG', 'T-3XG', 'T-4XG'];
+const childSize = ['T-2A', 'T-4A', 'T-6A', 'T-8A', 'T-10A', 'T-12A','T-14A', 'T-16A'];
 
 export function FormInputs() {
   const { t } = useTranslation();
-  const { state } = useCatalog();
+  const { state, dispatch } = useCatalog();
+  const maleTableRef = React.useRef<CatalogRef>(null);
+  const femaleTableRef = React.useRef<CatalogRef>(null);
+  const childishTableRef = React.useRef<CatalogRef>(null);
   const emailCompanyRef = React.useRef<HTMLInputElement>(null);
-  const nameCompanyRef = React.useRef<HTMLInputElement>(null);
-  const tabList = [
-    {
-      eventKey:'male',
-      title: t('MALE'),
-      list: state.priceTableMale,
-      sizes: ['T-PP', 'T-P', 'T-M', 'T-G', 'T-GG', 'T-XG', 'T-2XG', 'T-3XG', 'T-4XG'],
-      reference: 'priceTableMale'
-    },
-    {
-      eventKey:'female',
-      title: t('FEMALE'),
-      list: state.priceTableFemale,
-      sizes: ['T-PP', 'T-P', 'T-M', 'T-G', 'T-GG', 'T-XG', 'T-2XG', 'T-3XG', 'T-4XG'],
-      reference: 'priceTableFemale'
-    },
-    {
-      eventKey:'childish',
-      title: t('CHILDISH'),
-      list: state.priceTableChildish,
-      sizes: ['T-2A', 'T-4A', 'T-6A', 'T-8A', 'T-10A', 'T-12A','T-14A', 'T-16A'],
-      reference: 'priceTableChildish'
-    },
-  ];
+  const projectNameRef = React.useRef<HTMLInputElement>(null);
 
   return (
     <Container>
-      <Form>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          childishTableRef.current.submitEvent();
+          maleTableRef.current.submitEvent();
+          femaleTableRef.current.submitEvent();
+          dispatch({
+            type: 'setCompanyInfos',
+            payload: {
+              companyEmail: emailCompanyRef.current.value,
+              projectName: projectNameRef.current.value,
+            }
+          });
+        }}
+      >
         <Row>
           <Col>
             <Form.Group className="mb-3">
               <Form.Label>{t('BUDGET_IDENTIFICATION')}:</Form.Label>
               <Form.Control
-                ref={nameCompanyRef}
+                ref={projectNameRef}
                 type="text"
                 placeholder={t('UNTITLED')}
                 defaultValue={state.projectName}
@@ -61,12 +60,23 @@ export function FormInputs() {
           </Col>
         </Row>
         <Tabs defaultActiveKey="male" id="uncontrolled-tab-example" className="mb-3">
-          {tabList.map(({ list, sizes, reference, ...props }, i) => (
-            <Tab {...props} key={`${i}_`}>
-              <CatalogTable prices={list} sizes={sizes} reference={reference} />
-            </Tab>
-          ))}
+          <Tab eventKey="male" title={t('MALE')}>
+            <CatalogTable ref={maleTableRef} prices={state.priceTableMale} sizes={adultSizes} reference={'priceTableMale'} />
+          </Tab>
+          <Tab eventKey="female" title={t('FEMALE')}>
+            <CatalogTable ref={femaleTableRef} prices={state.priceTableFemale} sizes={adultSizes} reference={'priceTableFemale'} />
+          </Tab>
+          <Tab eventKey="childish" title={t('CHILDISH')}>
+            <CatalogTable ref={childishTableRef} prices={state.priceTableChildish} sizes={childSize} reference={'priceTableChildish'} />
+          </Tab>
         </Tabs>
+        <button 
+          className="btn btn-secondary d-flex gap-2 align-items-center m-auto"
+          type="submit"
+        >
+          <FontAwesomeIcon icon={faLink} />
+          {t('GENERATE_LINK')}
+        </button>
       </Form>
     </Container>
   );
