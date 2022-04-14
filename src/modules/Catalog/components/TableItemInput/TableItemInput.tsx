40396cb455dde62dba71b0df9891af82f6a72d3b
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-14 11:49:24 -0300

    feat: table item input component

---
+++ src/modules/Catalog/components/TableItemInput/TableItemInput.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentProps } from 'react';
+import InputMask from 'react-input-mask';
+import style from './style.module.css';
+
+type Props = {
+  onChange?: ComponentProps<'input'>['onChange']
+  onBlur?: ComponentProps<'input'>['onBlur']
+  value: number
+}
+
+export function TableItemInput({ onChange, value, onBlur }: Props) {
+  return (
+    <input 
+      onChange={onChange}
+      // value={value}
+      defaultValue={value}
+      type="number"
+      min={0}
+      onBlur={onBlur}
+      className={style.tableInput}/>
+  );
+}
