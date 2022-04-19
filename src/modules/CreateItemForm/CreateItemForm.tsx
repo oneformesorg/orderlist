6013fb6b44d536d0486f0c7bb2a741acdac7a90c
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-19 20:06:45 -0300

    refactor: add modal

--- src/modules/CreateItemForm/CreateItemForm.tsx
+++ src/modules/CreateItemForm/CreateItemForm.tsx
@@ -1,13 +1,17 @@
 import { faPlus, faPen, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'next-i18next';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ButtonGroup, Form, Col, Row, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { FormModal, FormModalRef } from './components/FormModal/FormModal';
 import { MicInput } from './components/MicInput/MicInput';
 
 export function CreateItemForm() {
   const { t, i18n } = useTranslation();
   const [hasWindow, setHasWindow] = useState(false);
+  const formModalRef = useRef<FormModalRef>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const numberRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     if(typeof window !== 'undefined'){
       setHasWindow(true);
@@ -19,7 +23,7 @@ export function CreateItemForm() {
       className='d-flex gap-2 flex-wrap align-items-center justify-content-center'
       onSubmit={e => e.preventDefault()}
     >
-      {hasWindow && <MicInput locale='pt-BR'/>}
+      {hasWindow && <MicInput locale={i18n.language}/>}
       <Row>
         <Col xs="6" sm="6" md="5" lg="5" xl="5">
           <Form.Group>
@@ -30,6 +34,7 @@ export function CreateItemForm() {
                 <FontAwesomeIcon icon={faUser} />
               </InputGroup.Text>
               <FormControl
+                ref={nameRef}
                 placeholder={`${t('EXAMPLE_ABBREV')} ${t('NAME_PLACEHOLDER')}`}
               />
             </InputGroup>
@@ -45,6 +50,7 @@ export function CreateItemForm() {
                 <FontAwesomeIcon icon={faPen} />
               </InputGroup.Text>
               <FormControl
+                ref={numberRef}
                 type='number'
                 placeholder={`${t('EXAMPLE_ABBREV')} 256`}
               />
@@ -57,7 +63,9 @@ export function CreateItemForm() {
             <Form.Label>{`${t('CLOTHES')}:`}</Form.Label>
             {/* Hide labels on mobile */}
             <ButtonGroup className="d-flex">
-              <Button>
+              <Button
+                onClick={() => formModalRef.current.showModal()}
+              >
                 <FontAwesomeIcon icon={faPlus} />
                 <span className="d-none d-sm-inline"> {t('ADD')}</span>
               </Button>
@@ -65,6 +73,9 @@ export function CreateItemForm() {
           </Form.Group>
         </Col>
       </Row>
+      <FormModal ref={formModalRef} sendForList={(cloth, gender, list) => {
+        console.log(cloth, gender, list, nameRef.current.value, numberRef.current.value);
+      }}/>
     </form>
   );
 }
