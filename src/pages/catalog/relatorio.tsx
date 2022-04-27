33845efe85039bc0e2d71d7fc42fd8144a6ee838
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-04-27 16:00:08 -0300

    perf: remove ssr of reportInfo component

--- src/pages/catalog/relatorio.tsx
+++ src/pages/catalog/relatorio.tsx
@@ -2,7 +2,6 @@ import { GetStaticProps, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Menu } from '@modules/Menu/Menu';
 import { CatalogStateProvider } from '@shared/Catalog/context/catalog';
-import { ReportInfos } from '@modules/ReportInfos/ReportInfos';
 import styles from '@styles/relatorio.module.css';
 import { ImportCSVButton } from '@modules/ImportCSVButton/ImportCSVButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,11 +9,21 @@ import { faPrint } from '@fortawesome/free-solid-svg-icons';
 import { AddImage, ImageState } from '@modules/AddImage/AddImage';
 import { createContext, Dispatch, SetStateAction, useState } from 'react';
 import Head from 'next/head';
+import { ListActionProvider } from '@shared/List';
+import dynamic from 'next/dynamic';
+import { ReportInfosProps } from '@modules/ReportInfos/ReportInfos';
 
 type ImagesContext = {
   images: ImageState
   setImages: Dispatch<SetStateAction<ImageState>>
 }
+
+// !dynamic import for remove ssr
+const ReportInfosWithoutSSR = dynamic<ReportInfosProps>(
+  import('@modules/ReportInfos/ReportInfos').then((mod) => mod.ReportInfos),
+  { ssr: false }
+);
+
 export const imagesCardContext = createContext<ImagesContext>(null);
 const Relatorio:NextPage = () => {
   const [images, setImages] = useState<ImageState>([]);
@@ -25,33 +34,35 @@ const Relatorio:NextPage = () => {
         <title>List</title>
       </Head>
       <Menu />
-      <imagesCardContext.Provider value={{
-        images, setImages
-      }}>
-        <section className={`${styles.relatorioButtons} container-lg d-flex justify-content-end gap-2 px-5 py-2 border-bottom mb-3`}>
-          <AddImage />
+      <ListActionProvider>
+        <imagesCardContext.Provider value={{
+          images, setImages
+        }}>
+          <section className={`${styles.relatorioButtons} container-lg d-flex justify-content-end gap-2 px-5 py-2 border-bottom mb-3`}>
+            <AddImage />
+            <CatalogStateProvider>
+              <ImportCSVButton />
+            </CatalogStateProvider>
+            <button 
+              onClick={() =>{
+                window.print();
+              }}
+              className='btn btn-info btn-sm text-light'
+            >
+              <FontAwesomeIcon icon={faPrint} /> Print
+            </button>
+          </section>
           <CatalogStateProvider>
-            <ImportCSVButton />
+            <ReportInfosWithoutSSR
+              onDelete={(id) => {
+                setImages(old => (
+                  old.filter((item, index) => index !== id)
+                ));
+              }}
+            />
           </CatalogStateProvider>
-          <button 
-            onClick={() =>{
-              window.print();
-            }}
-            className='btn btn-info btn-sm text-light'
-          >
-            <FontAwesomeIcon icon={faPrint} /> Print
-          </button>
-        </section>
-        <CatalogStateProvider>
-          <ReportInfos 
-            onDelete={(id) => {
-              setImages(old => (
-                old.filter((item, index) => index !== id)
-              ));
-            }}
-          />
-        </CatalogStateProvider>
-      </imagesCardContext.Provider>
+        </imagesCardContext.Provider>
+      </ListActionProvider>
     </>
   );
 }; 
