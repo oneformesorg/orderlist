ef3bf83733408ed8457e84f183246f67c0dada1e
joaovictor6 <santosjoao.dev@gmail.com>
2022-05-06 19:33:03 -0300

    refactor: improve csv feature

--- src/shared/csv/csvReader.ts
+++ src/shared/csv/csvReader.ts
@@ -8,28 +8,38 @@ const genderCSV = {
 };
 
 const sizesForTranslate: [string, [string, string, string]][] = [
-  ['T-PP', ['XS', 'XS', 'PP']],
-  ['T-P', ['S', 'S', 'P']],
-  ['T-M', ['M', 'M', 'M']],
-  ['T-G', ['L', 'L', 'G']],
-  ['T-GG', ['XL', 'XL', 'GG']],
-  ['T-XG', ['XXL', 'XXL', 'XG']],
-  ['T-2XG', ['2XL', '2XL', '2XG']],
-  ['T-3XG', ['3XL', '3XL', '3XG']],
-  ['T-4XG', ['4XL', '4XL', '4XG']],
-  ['T-2A', ['2-3', '2 años', '2 anos']],
-  ['T-4A', ['4-5', '4 años', '4 anos']],
-  ['T-6A', ['6-6X', '6 años', '6 anos']],
-  ['T-8A', ['7-8', '8 años', '8 anos']],
-  ['T-10A', ['10', '10 años', '10 anos']],
-  ['T-12A', ['12', '12 años', '12 anos']],
-  ['T-14A', ['14', '14 años', '14 anos']],
-  ['T-16A', ['16', '16 años', '16 anos']],
+  ['PP', ['XS', 'XS', 'PP']],
+  ['P', ['S', 'S', 'P']],
+  ['M', ['M', 'M', 'M']],
+  ['G', ['L', 'L', 'G']],
+  ['GG', ['XL', 'XL', 'GG']],
+  ['XG', ['XXL', 'XXL', 'XG']],
+  ['2XG', ['2XL', '2XL', '2XG']],
+  ['3XG', ['3XL', '3XL', '3XG']],
+  ['4XG', ['4XL', '4XL', '4XG']],
+  ['PP', ['BL-XS', 'BL-XS', 'BL-PP']],
+  ['P', ['BL-S', 'BL-S', 'BL-P']],
+  ['M', ['BL-M', 'BL-M', 'BL-M']],
+  ['G', ['BL-L', 'BL-L', 'BL-G']],
+  ['GG', ['BL-XL', 'BL-XL', 'BL-GG']],
+  ['XG', ['BL-XXL', 'BL-XXL', 'BL-XG']],
+  ['2XG', ['BL-2XL', 'BL-2XL', 'BL-2XG']],
+  ['3XG', ['BL-3XL', 'BL-3XL', 'BL-3XG']],
+  ['4XG', ['BL-4XL', 'BL-4XL', 'BL-4XG']],
+  ['CHILDISH-2A', ['2-3', '2 años', '2 anos']],
+  ['CHILDISH-4A', ['4-5', '4 años', '4 anos']],
+  ['CHILDISH-6A', ['6-6X', '6 años', '6 anos']],
+  ['CHILDISH-8A', ['7-8', '8 años', '8 anos']],
+  ['CHILDISH-10A', ['10', '10 años', '10 anos']],
+  ['CHILDISH-12A', ['12', '12 años', '12 anos']],
+  ['CHILDISH-14A', ['14', '14 años', '14 anos']],
+  ['CHILDISH-16A', ['16', '16 años', '16 anos']],
   
 ];
 
 const translateSizeForSystem = (size: string[] | string) => {
   const sizeNormalized = typeof size === 'string' ? size : size.join('-');
+  console.log(size, sizeNormalized);
   const sizeForSystem = sizesForTranslate.filter(([, langs]) => {
     const [en, es, pt] = langs;
     return sizeNormalized === en || sizeNormalized === es || sizeNormalized === pt;
@@ -43,6 +53,7 @@ const translateSizeForSystem = (size: string[] | string) => {
  */
 const sanitizeClothes = (cloth: string[]): GenericClothStructure => {
   const sanitizedList = cloth.splice(3).map(item => item.split('=')[1]);
+  // translateSizeForSystem(sanitizedList[1].split(/-/).splice(1));
   return {
     tshirt: {
       quantity: Number(sanitizedList[0].split(/-/)[0]),
