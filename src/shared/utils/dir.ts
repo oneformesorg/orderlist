6cb54997ff6cf90f8e44b0e818e661a42d1f670c
joaovictor6 <santosjoao.dev@gmail.com>
2022-05-30 00:33:48 -0300

    refactor: change vercel config

--- src/shared/utils/dir.ts
+++ src/shared/utils/dir.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export function dir(filename?:string){
   if(filename){
-    return path.join(__dirname,`_temp/${filename}.json`);
+    return path.join(__dirname,`.output/_temp/${filename}.json`);
   }
-  return path.join(__dirname,'_temp');
+  return path.join(__dirname,'.output/_temp');
 }
